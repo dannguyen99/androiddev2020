fbaa8b11e3f1cc72b1a59983abe15d6477d05407
dannguyen <dannguyen0801@gmail.com>
2019-11-11 14:57:02 +0700

    update on practical work 2

--- app/src/main/java/com/example/usthweather/WeatherActivity.java
+++ app/src/main/java/com/example/usthweather/WeatherActivity.java
@@ -2,13 +2,45 @@ package com.example.usthweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 
 public class WeatherActivity extends AppCompatActivity {
-
+    private final String tag = "status";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i(tag, "on start");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(tag, "on resume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(tag, "on pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(tag, "on stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(tag, "on destroy");
     }
 }
